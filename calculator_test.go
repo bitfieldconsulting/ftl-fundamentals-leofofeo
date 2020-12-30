98e879e1bce3ad4e79f9b21028d90a35ffcb8444
leofofeo <leo.a.rubiano@gmail.com>
2020-12-30 16:36:04 -0600

    divide function and tests

--- calculator_test.go
+++ calculator_test.go
@@ -6,9 +6,10 @@ import (
 )
 
 type testCase struct {
-	a, b float64
-	want float64
-	name string
+	a, b        float64
+	want        float64
+	name        string
+	errExpected bool
 }
 
 func TestAdd(t *testing.T) {
@@ -75,5 +76,82 @@ func TestMultiply(t *testing.T) {
 			t.Errorf("%s", tc.name)
 			t.Errorf("want %f, got %f", tc.want, got)
 		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{
+			a:           2,
+			b:           2,
+			want:        1,
+			name:        "Divide 2 and 2",
+			errExpected: false,
+		},
+		{
+			a:           1,
+			b:           -1,
+			want:        -1,
+			name:        "Divide a positive and a negative",
+			errExpected: false,
+		},
+		{
+			a:           10,
+			b:           100,
+			want:        .1,
+			name:        "Divide a smaller number by a larger number",
+			errExpected: false,
+		},
+		{
+			a:           -40,
+			b:           .5,
+			want:        -80,
+			name:        "Divide a negative integer by a positive decimal",
+			errExpected: false,
+		},
+		{
+			a:           6,
+			b:           0,
+			want:        0,
+			name:        "Divide a positive integer by zero",
+			errExpected: true,
+		},
+		{
+			a:           0,
+			b:           10,
+			want:        0,
+			name:        "Divide zero by an integer",
+			errExpected: false,
+		},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if tc.errExpected {
+			if err != nil {
+				if tc.want != got {
+					t.Errorf("Error for the following test: ")
+					t.Errorf("%s", tc.name)
+					t.Errorf("want %f, got %f", tc.want, got)
+				}
+			} else {
+				t.Errorf("Error for the following test: ")
+				t.Errorf("%s", tc.name)
+				t.Errorf("Expected error but got none.")
+			}
+		} else {
+			if err != nil {
+				t.Errorf("Error for the following test: ")
+				t.Errorf("%s", tc.name)
+				t.Errorf("Got error - invalid inputs")
+			} else {
+				if tc.want != got {
+					t.Errorf("Error for the following test: ")
+					t.Errorf("%s", tc.name)
+					t.Errorf("want %f, got %f", tc.want, got)
+				}
+			}
+		}
+
 	}
 }
