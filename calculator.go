5435882bcf10f48459a107387f63f9f445e709e3
leofofeo <leo.a.rubiano@gmail.com>
2020-12-30 15:30:45 -0600

    Add TestMultiply and the Multiply function

--- calculator.go
+++ calculator.go
@@ -11,3 +11,8 @@ func Add(a, b float64) float64 {
 func Subtract(a, b float64) float64 {
 	return a - b
 }
+
+// Multiply takes two numbers and returns their product.
+func Multiply(a, b float64) float64 {
+	return a * b
+}
